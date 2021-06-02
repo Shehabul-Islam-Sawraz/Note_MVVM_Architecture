da1cc8593d7d4df9a6a7fa3be8c378d06cdd14f7
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2021-06-02 15:07:30 +0600

    Note item design's xml files added

--- app/src/main/java/com/example/notesaver/MainActivity.java
+++ app/src/main/java/com/example/notesaver/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.notesaver;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                // This function will be called when we start observing. So it will be called at
+                // the moment the app is instantiated.
+            }
+        });
     }
 }
