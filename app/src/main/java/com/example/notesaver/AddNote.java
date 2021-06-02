2a35a2488bb57861184ccc6f98ea9a0b1ff6c6a2
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2021-06-03 01:14:32 +0600

    Functionalities added for saving a new note & showing it to the screen instantly

---
+++ app/src/main/java/com/example/notesaver/AddNote.java
@@ -0,0 +1,81 @@
+package com.example.notesaver;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.Toast;
+
+public class AddNote extends AppCompatActivity {
+
+    public static final String EXTRA_TITLE = "com.example.notesaver.EXTRA_TITLE";
+    public static final String EXTRA_DESCRIPTION = "com.example.notesaver.EXTRA_DESCRIPTION";
+    public static final String EXTRA_PRIORITY = "com.example.notesaver.EXTRA_PRIORITY";
+    public static final String NOTE_SAVED = "com.example.notesaver.NOTE_SAVED";
+
+
+    private EditText title;
+    private EditText description;
+    private NumberPicker priority;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+
+        title = findViewById(R.id.add_note_title);
+        description = findViewById(R.id.add_note_description);
+        priority = findViewById(R.id.add_note_number_picker);
+
+        priority.setMinValue(1);
+        priority.setMaxValue(10);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+        setTitle("Add New Note");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.add_note_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.save_note:
+                saveNote();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void saveNote() {
+        String note_title = title.getText().toString();
+        String note_description = description.getText().toString();
+        int note_priority = priority.getValue();
+        if(note_title.trim().isEmpty()){
+            Toast.makeText(this, "Please add a title", Toast.LENGTH_SHORT).show();
+        }
+        else if(note_description.trim().isEmpty()){
+            Toast.makeText(this, "Please add a description", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Intent intent = new Intent();
+            intent.putExtra(EXTRA_TITLE,note_title);
+            intent.putExtra(EXTRA_DESCRIPTION,note_description);
+            intent.putExtra(EXTRA_PRIORITY,note_priority);
+            intent.putExtra(NOTE_SAVED,200);
+            setResult(RESULT_OK,intent);
+            finish();
+        }
+    }
+}
--- app/src/main/java/com/example/notesaver/MainActivity.java
+++ app/src/main/java/com/example/notesaver/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.notesaver;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
@@ -15,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     private NoteViewModel noteViewModel;
     private RecyclerView recyclerView;
     private NoteAdapter noteAdapter;
+    private FloatingActionButton addNoteButton;
+    private ActivityResultLauncher<Intent> addNoteActivityResultLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,33 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         noteAdapter = new NoteAdapter();
         recyclerView.setAdapter(noteAdapter);
+        addNoteButton = findViewById(R.id.note_add);
+
+        addNoteActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result ->  {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        String title = data.getStringExtra(AddNote.EXTRA_TITLE);
+                        String description = data.getStringExtra(AddNote.EXTRA_DESCRIPTION);
+                        int priority = data.getIntExtra(AddNote.EXTRA_PRIORITY, 1);
+                        int saved = data.getIntExtra(AddNote.NOTE_SAVED,1);
+                        if (saved == 200) {
+                            Note note = new Note(title, description, priority);
+                            noteViewModel.insert(note);
+                            Toast.makeText(MainActivity.this, "Note Saved", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "Can't Save the Note", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+        addNoteButton.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, AddNote.class);
+            //startActivity(intent);
+            addNoteActivityResultLauncher.launch(intent);
+        });
         noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
@@ -34,4 +71,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    /*@Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==RESULT_OK){
+            String title = data.getStringExtra(AddNote.EXTRA_TITLE);
+            String description = data.getStringExtra(AddNote.EXTRA_DESCRIPTION);
+            int priority = data.getIntExtra(AddNote.EXTRA_PRIORITY,1);
+            Note note = new Note(title, description, priority);
+            noteViewModel.insert(note);
+            Toast.makeText(this, "Note Saved", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(this, "Can't Save the Note", Toast.LENGTH_SHORT).show();
+        }
+    }*/
+
 }
